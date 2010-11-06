dfda61a6b64e6eeac0e5ba7d173a03ccf35f769b
Dan Mohl <dmohl@yahoo.com>
2010-11-06 11:48:24 -0500

    Created

---
+++ Async/Program.fs
@@ -0,0 +1,27 @@
+﻿open System
+open System.IO
+open System.Net
+
+let getHtml url printStrategy =
+    async { let request =  WebRequest.Create(Uri url)
+            use! response = request.AsyncGetResponse()
+            use stream = response.GetResponseStream()
+            use reader = new StreamReader(stream)
+            let contents = reader.ReadToEnd()
+            do printStrategy url contents
+            return contents }
+ 
+let sites = ["http://www.bing.com";
+             "http://www.google.com";
+             "http://www.yahoo.com";
+             "http://msdn.microsoft.com/en-us/fsharp/default.aspx"]
+
+let printStrategy url (contents:string) =  
+    printfn "%s - HTML Length %d" url contents.Length
+
+let sitesHtml = Async.Parallel [for site in sites -> getHtml site printStrategy]
+                |> Async.RunSynchronously
+
+do printfn "\r\nTotal Character length = %d\r\nPress any key to continue" sitesHtml.Length
+
+do Console.ReadLine() |> ignore
---
+++ CSharpAsync/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace CSharpAsync
+{
+    class Program
+    {
+        static async Task<string> GetHtml(string url, Action<string, string> printStrategy)
+        {
+            var request = WebRequest.Create(new Uri(url));
+            using (var response = await request.GetResponseAsync())
+            {
+                using (var stream = response.GetResponseStream())
+                {
+                    using (var reader = new StreamReader(stream))
+                    {
+                        var contents = reader.ReadToEnd();
+                        printStrategy.Invoke(url, contents);
+                        return contents;
+                    }
+                }
+            }        
+        }
+
+        static async Task ProcessSites(string[] sites, Action<string, string> printStrategy)
+        {
+            var sitesHtml = 
+                await TaskEx.WhenAll(
+                    sites.Select(site => GetHtml(site, printStrategy)));
+            Console.WriteLine("\r\nTotal Character length = {0}\r\nPress any key to continue", 
+                sitesHtml.Length);
+        }
+
+        static void Main(string[] args)
+        {
+            var sites = new[] { "http://www.bing.com",
+                            "http://www.google.com",
+                            "http://www.yahoo.com",
+                            "http://msdn.microsoft.com/en-us/fsharp/default.aspx" };
+
+            Action<string, string> printStrategy =
+                (url, contents) =>
+                    Console.WriteLine("{0} - HTML Length {1}", url, contents.Length);
+            
+            ProcessSites(sites, printStrategy).Wait();
+
+            Console.ReadLine();
+        }
+    }
+}
